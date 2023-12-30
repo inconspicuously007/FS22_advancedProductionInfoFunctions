95afa13bff1fde8949a3e28f65fd25711c6d3c9e
inconspicuously007 <58066449+inconspicuously007@users.noreply.github.com>
2023-12-30 09:34:58 +0100

    BugFix
    BugFix

--- scripts/AdvancedSiloDialog.lua
+++ scripts/AdvancedSiloDialog.lua
@@ -1,13 +1,20 @@
 AdvancedSiloDialog = {}
 
 function AdvancedSiloDialog:setFillLevels(superFunc, fillLevels, hasInfiniteCapacity)
-	self.fillLevels = fillLevels
+	self.fillLevels = {}
+	
+	--print("Debug: self.fillLevels")
+	--DebugUtil.printTableRecursively(self.fillLevels,"_",0,3)
+	
 	self.fillTypeMapping = {}
 	local fillTypesTable = {}
+	local fillTypesTableAll = {}
+	local fillTypeMappingAll = {}
+	local fillTypeMappingFill = {}
 	local selectedId = 1
 	local numFillLevels = 1
 
-	for fillTypeIndex, _ in pairs(fillLevels) do
+	for fillTypeIndex, fillLevel in pairs(fillLevels) do
 		local fillType = g_fillTypeManager:getFillTypeByIndex(fillTypeIndex)
 		local level = Utils.getNoNil(fillLevels[fillTypeIndex], 0)
 		local name = nil
@@ -18,17 +25,26 @@ function AdvancedSiloDialog:setFillLevels(superFunc, fillLevels, hasInfiniteCapa
 			name = string.format("%s %s", fillType.title, g_i18n:formatFluid(level))
 		end
 		
+		table.insert(fillTypeMappingAll, fillTypeIndex)	
+		table.insert(fillTypesTableAll, name)	
 		if level > 0 then
-			table.insert(fillTypesTable, name)
-			table.insert(self.fillTypeMapping, fillTypeIndex)		
+			self.fillLevels[fillTypeIndex] = fillLevel
+			table.insert(fillTypesTable, name)	
+			table.insert(fillTypeMappingFill, fillTypeIndex)			
 			if fillTypeIndex == self.lastSelectedFillType then
 				selectedId = numFillLevels
 			end
 			numFillLevels = numFillLevels + 1
 		end		
 	end
-
-	self.fillTypesElement:setTexts(fillTypesTable)
+	if numFillLevels < 2 then
+		self.fillLevels = fillLevels
+		self.fillTypesElement:setTexts(fillTypesTableAll)
+		self.fillTypeMapping = fillTypeMappingAll
+	else
+		self.fillTypesElement:setTexts(fillTypesTable)
+		self.fillTypeMapping = fillTypeMappingFill
+	end
 	self.fillTypesElement:setState(selectedId, true)
 end
 
